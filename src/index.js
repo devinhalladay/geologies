9c22dbfffa70bd8cb52d4ffd6c1164fa72dc264c
Devin Halladay <devin@devinhalladay.com>
2022-05-24 22:32:32 -0400

    some more initial setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/main.scss';
+import './styles/main.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
