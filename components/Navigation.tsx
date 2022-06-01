b319a8b9d46b58ae49431e064d4c6d52e20d8fbd
Devin Halladay <devin@devinhalladay.com>
2022-06-01 16:52:47 -0400

    update nav font

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -8,7 +8,7 @@ function Navigation() {
   const hasLibrary = useFeature('library');
 
   return (
-    <header className="relative h-[92px] sm:h-14 border-b border-dashed border-moss/40 flex items-start sm:items-center mb-10">
+    <header className="relative h-[92px] sm:h-14 border-b border-dashed border-moss/40 flex items-start sm:items-center mb-10 font-sans">
       <Masthead />
       <nav className="absolute pt-[32px] sm:pt-0 left-0 right-0 top-0 bottom-0 grow items-center flex justify-center">
         <ul className="flex items-center gap-2 uppercase tracking-wider smallcaps text-xs">
