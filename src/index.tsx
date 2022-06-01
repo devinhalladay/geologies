833ab2831e1621ac9442bcb8ed1ee667018cf109
Devin Halladay <devin@devinhalladay.com>
2022-06-01 00:20:35 -0400

    Remove unused deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,20 @@
+import { FlagsProvider } from 'flagged';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/main.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { FlagsProvider } from 'flagged';
 import features from './constants/features';
-import { HomebaseProvider } from 'homebase-react';
+import reportWebVitals from './reportWebVitals';
+import './styles/main.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <FlagsProvider features={features}>
-      <HomebaseProvider
-        config={{ initialData: [{ counter: { id: 1, count: 0 } }] }}
-      >
-        <App />
-      </HomebaseProvider>
+      <App />
     </FlagsProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Documentation: https://bit.ly/CRA-vitals
 reportWebVitals();
