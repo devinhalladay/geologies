dea0acf207b94388cee6f35623edbb4e5886a1fa
Devin Halladay <devin@devinhalladay.com>
2022-06-03 11:15:24 -0600

    Prototype scroll interaction with array of refs

--- pages/library/[id].tsx
+++ pages/library/[id].tsx
@@ -1,4 +1,6 @@
 import { useAnimation } from 'framer-motion';
+import { animate as framer, AnimationControls, motion } from 'framer-motion';
+
 import { Interweave, MatcherInterface, MatchResponse } from 'interweave';
 import { useEffect, useRef, useState } from 'react';
 import { useLocalstorage } from 'rooks';
@@ -11,77 +13,35 @@ import { useBooks, useHighlights } from '../../lib/readwise';
 import { Book, Highlight } from '../../lib/readwise/types';
 import { escapeForRegExp, usePreventGestureDefault } from '../../utils';
 import { ReaderResponse } from '../api/reader';
+import { forwardRef } from 'react';
 
 const fetchArticle = async (book: Book): Promise<ReaderResponse> => {
   const reader = await fetch(`/api/reader?url=${book.source_url}`);
   const markup = await reader.json();
-  console.log(markup);
-
   return markup;
 };
+
+const HighlightSpan = forwardRef<HTMLSpanElement, any>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <span ref={ref} {...rest} className="highlight">
+        {children}
+      </span>
+    );
+  }
+);
 
 function Article() {
   const { value: token } = useLocalstorage(READWISE_TOKEN_LOCALSTORAGE_KEY);
   const { bookmarks, loading } = useHighlights();
   const { books, loading: loadingBooks, error } = useBooks(token);
   const highlightRefs = useRef<HTMLSpanElement[]>([]);
+  const pageRef = useRef<HTMLDivElement>();
 
-  const matcher = (bookmark: Highlight): MatcherInterface => {
-    return {
-      inverseName: 'noFoo',
-      propName: 'foo',
-      match(string): MatchResponse<any> {
-        const regex = new RegExp(escapeForRegExp(bookmark.text));
-        const result = string.match(regex);
-
-        if (!result) {
-          return null;
-        }
-
-        return {
-          index: result.index!,
-          length: result[0].length,
-          match: result[0],
-          className: 'highlight',
-          valid: true,
-        };
-      },
-      createElement(children, props) {
-        return (
-          <span
-            {...props}
-            ref={(el) =>
-              (highlightRefs.current[highlightRefs.current.length + 1] = el)
-            }
-          >
-            {children}
-          </span>
-        );
-      },
-      asTag() {
-        return 'span';
-      },
-    };
-  };
-
-  const matchers = bookmarks.map((bookmark, i) => {
-    return matcher(bookmark);
-  });
-
-  const book = books.find(({ id }) => id === bookmarks[0].book_id);
-
-  const [pan, setPan] = useState(false);
-  const controls = useAnimation();
-
-  const [article, setArticle] = useState<string>(null);
-
+  // log highlightRefs to the console when it updates
   useEffect(() => {
-    if (book) {
-      fetchArticle(book).then((b) => {
-        setArticle(b.markup);
-      });
-    }
-  }, [book]);
+    highlightRefs.current = highlightRefs.current.filter((e) => e);
+  }, [highlightRefs.current]);
 
   usePreventGestureDefault();
 
@@ -110,6 +70,121 @@ function Article() {
     }
   );
 
+  const matcher = (bookmark: Highlight): MatcherInterface => {
+    return {
+      inverseName: 'noFoo',
+      propName: 'foo',
+      match(string): MatchResponse<any> {
+        const regex = new RegExp(escapeForRegExp(bookmark.text));
+        const result = string.match(regex);
+
+        if (!result) {
+          return null;
+        }
+
+        return {
+          index: result.index!,
+          length: result[0].length,
+          match: result[0],
+          className: 'highlight',
+          valid: true,
+        };
+      },
+      createElement(children, props) {
+        const index = highlightRefs.current.length;
+
+        return (
+          <HighlightSpan
+            ref={(el) =>
+              (highlightRefs.current[parseInt(bookmark.location)] = el)
+            }
+            key={index}
+            {...props}
+          >
+            {children}
+          </HighlightSpan>
+        );
+      },
+      asTag() {
+        return 'span';
+      },
+    };
+  };
+
+  const matchers = bookmarks.map((bookmark, i) => {
+    return matcher(bookmark);
+  });
+
+  const book = books.find(({ id }) => id === bookmarks[0].book_id);
+
+  const [pan, setPan] = useState(false);
+  const controls = useAnimation();
+
+  const [article, setArticle] = useState<string>(null);
+
+  useEffect(() => {
+    if (book) {
+      fetchArticle(book).then((b) => {
+        setArticle(b.markup);
+      });
+    }
+  }, [book]);
+
+  useEffect(() => {
+    // if (highlightRefs.current.length) {
+    //   const newRef = highlightRefs.current.map((ref, i) => {
+    //     let topPos = ref.offsetTop + ref.clientHeight;
+
+    //     if (pan) {
+    //       framer(document.body.scrollTop, topPos, {
+    //         onUpdate: (top) =>
+    //           document.body.scrollTo({ top, behavior: 'smooth' }),
+    //       });
+    //     }
+
+    //     return ref;
+    //   });
+
+    //   highlightRefs.current = newRef;
+    // }
+    if (document.documentElement) {
+      if (highlightRefs.current.length) {
+        const ref = highlightRefs.current[0];
+
+        let topPos = ref.offsetTop + ref.clientHeight;
+
+        if (pan) {
+          framer(document.documentElement.scrollTop, topPos, {
+            onUpdate: (top) => {
+              console.log('test');
+
+              document.documentElement.scrollTo({
+                top,
+                behavior: 'smooth',
+              });
+            },
+          });
+        }
+      }
+    }
+  }, [pan]);
+
+  useEffect(() => {
+    if (highlightRefs.current.length) {
+      console.log(highlightRefs.current);
+
+      let topPos =
+        highlightRefs.current[0].offsetTop +
+        highlightRefs.current[0].clientHeight;
+      if (pan) {
+        framer(document.body.scrollTop, topPos, {
+          onUpdate: (top) =>
+            document.body.scrollTo({ top, behavior: 'smooth' }),
+        });
+      }
+    }
+  }, [pan, highlightRefs]);
+
   return loading || loadingBooks || !article ? (
     <div>Loading…</div>
   ) : (
@@ -137,7 +212,11 @@ function Article() {
           />
         </div>
       </div>
-      <Interweave content={article} matchers={matchers} />;
+      <div {...bind()}>
+        <motion.div ref={pageRef} animate={controls}>
+          <Interweave content={article} matchers={matchers} />;
+        </motion.div>
+      </div>
       {/* {bookmarks.map((bookmark, i) => {
         return i < 5 ? (
           <Page
